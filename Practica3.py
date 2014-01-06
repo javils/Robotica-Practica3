9b7fa64361550068492aa67dd8f3f9a619c46980
Javils <javiluke93@gmail.com>
2014-01-06 04:11:07 +0100

    Comienza de nuevo el codigo a ver si consigo algo de esta forma

--- Practica3.py
+++ Practica3.py
@@ -1,108 +1,83 @@
 from pyrobot.brain import Brain
 import random
 
-kMax_steps = 100
-kMax_individuos = 30
-kMax_generaciones = 100
-individuo = 0
-gen = 0
-poblacion = []
-valoracionPoblacion = []
+class Individuo :
+  def __init__(self, IndId):
+    # Constantes
+    self.CONTROL_BORROSO_SIZE = 12 # tamaño del array controlBorroso
+    # Variables
+    self.id = IndId #Id para diferenciar un individuo de otro
+    self.calidad = 0 # calidad del individuo
+    self.controlBorroso = initControlBorroso() # array con la funcion de pertenencia [X,X,X,Y,Y,Y,Z,Z,Z,Z,Z,Z] X = Error Y = D.Error Z = Salida
+    self.probabilidad = 0 #probabilidad de ser elegido en la mutación
 
-class Practica3(Brain):
+  def initControlBorroso(self):
+    # Error
+    aux = [random.random() for col in range(3)]
+    aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
+    self.controlBorroso[0] = aux[0]
+    self.controlBorroso[1] = aux[1]
+    self.controlBorroso[2] = aux[2]
 
+    # Derivada del error
+    aux = [random.random() for col in range(3)]
+    aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
+    self.controlBorroso[3] = aux[0]
+    self.controlBorroso[4] = aux[1]
+    self.controlBorroso[5] = aux[2]
+
+    # Salida
+    aux = [random.random() for col in range(6)]
+    aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
+    self.controlBorroso[6] = aux[0]
+    self.controlBorroso[7] = aux[1]
+    self.controlBorroso[8] = aux[2]
+    self.controlBorroso[9] = aux[3]
+    self.controlBorroso[10] = aux[4]
+    self.controlBorroso[11] = aux[5]
+
+
+class Control(Brain):
+  
+  def __init__(self):
+
+    # Constantes
+    self.MAX_GEN = 100
+    self.MAX_ITR = 1500
+    self.MAX_IND = 20
+
+    # Variables
+    self.itr = 0; #numero de iteraciones que ha relizado el robot hasta ahora
+    self.gen = 0; #generacion en la que estamos
+    self.ind = 0; #individuo en el que estamos
+    self.poblacion = range(0, MAX_IND)
+    self.elite = Individuo(-1)
+    probTotal = 0
+
+    # Inicializamos la primera población
+    for i in range MAX_IND:
+      self.poblacion[i] = Individuo(i)
+      self.poblacion[i].calidad = self.setCalidad(1) # Para que no crashe por dividir entre 0 mas adelante
+      probTotal = probTotal + self.poblacion[i].calidad
+
+      # Elitismo
+      if (i == 0):
+        self.elite = self.poblacion[i]
+      elif (self.elite.calidad < self.poblacion[i].calidad)
+        self.elite = self.poblacion[i]
+
+    # Asignamos la probabilidad que tiene de ser elegido cuando se vaya a mutar.
+    for i in range MAX_IND:
+      self.poblacion[i].probabilidad = float((self.poblacion[i].calidad)/probTotal)
 
   def setup(self):
-      done = 0
-      iter = 0
-      poblacion = self.genera_primera_poblacion()
-      self.coloca_individuo(individuo)
+      
 
   def step(self):
-      if iter == kMax_steps or luz_alcanzada:
-          self.asigna_calidad_individuo(individuo, iter)
-          individuo = individuo + 1
-          if individuo == kMax_individuos:
-              individuo = 0
-              gen = gen + 1
-              if gen == kMax_generaciones:
-                  done = 1
-              else:
-                  self.generar_nueva_poblacion
-          self.coloca_individuo(individuo)
-          iter = 0
-      else:
-          t,r = determineMove(individuo)
-          self.robot.move(t,r)
-          iter = iter + 1
+      
 
-  def genera_primera_poblacion(self):
-    array = []
-    genes = {"ErrorA":0,"ErrorB":0,"ErrorC":0,"ErrorD":0,"DerErrorA":0,"DerErrorB":0,"DerErrorC":0,"DerErrorD":0,"Salida1":0,"Salida2":0,"Salida3":0,"Salida4":0,"Salida5":0,"Salida6":0}
-
-    for i in 0..kMax_Individuos:
-        genes["ErrorA"] = random.random
-        genes["ErrorB"] = genes["ErrorA"] + random.random
-        genes["ErrorC"] = genes["ErrorB"] + random.random
-        genes["ErrorD"] = genes["ErrorC"] + random.random
-
-        genes["DerErrorA"] = random.random
-        genes["DerErrorB"] = genes["DerErrorA"] + random.random
-        genes["DerErrorC"] = genes["DerErrorB"] + random.random
-        genes["DerErrorD"] = genes["DerErrorC"] + random.random
-
-        genes["Salida1"] = random.random
-        genes["Salida2"] = genes["Salida1"] + random.random
-        genes["Salida3"] = genes["Salida2"] + random.random
-        genes["Salida4"] = genes["Salida3"] + random.random
-        genes["Salida5"] = genes["Salida4"] + random.random
-        genes["Salida6"] = genes["Salida5"] + random.random
-
-        array[i] = genes
-
-    return array
-
-
-def asigna_calidad_individuo(self,individuo,iter):
-    if iter == kMax_steps:
-        valoracionPoblacion[individuo] = 0
-    else:
-        valoracionPoblacion[individuo] = 1 / iter
-
-
-def genera_nueva_poblacion(self):
-    individuo = mejorIndividuo()
-
-    array = []
-    genes = {"ErrorA":0,"ErrorB":0,"ErrorC":0,"ErrorD":0,"DerErrorA":0,"DerErrorB":0,"DerErrorC":0,"DerErrorD":0,"Salida1":0,"Salida2":0,"Salida3":0,"Salida4":0,"Salida5":0,"Salida6":0}
-
-    for i in 0..kMax_Individuos:
-
-        #Falta comprobar si el valor es valido
-
-        genes["ErrorA"] = poblacion[individuo]["ErrorA"] + random.gauss(0,1)
-        genes["ErrorB"] = poblacion[individuo]["ErrorB"] + random.gauss(0,1)
-        genes["ErrorC"] = poblacion[individuo]["ErrorC"] + random.gauss(0,1)
-        genes["ErrorD"] = poblacion[individuo]["ErrorD"] + random.gauss(0,1)
-
-        genes["DerErrorA"] = poblacion[individuo]["DerErrorA"] + random.gauss(0,1)
-        genes["DerErrorB"] = poblacion[individuo]["DerErrorB"] + random.gauss(0,1)
-        genes["DerErrorC"] = poblacion[individuo]["DerErrorC"] + random.gauss(0,1)
-        genes["DerErrorD"] = poblacion[individuo]["DerErrorD"] + random.gauss(0,1)
-
-        genes["Salida1"] = random.random
-        genes["Salida2"] = genes["Salida1"] + random.random
-        genes["Salida3"] = genes["Salida2"] + random.random
-        genes["Salida4"] = genes["Salida3"] + random.random
-        genes["Salida5"] = genes["Salida4"] + random.random
-        genes["Salida6"] = genes["Salida5"] + random.random
-
-        array[i] = genes
-
-    return array
-
-
-def INIT(engine):
-   assert (engine.robot.requires("range-sensor") and
+  
+  def INIT(engine):
+    assert (engine.robot.requires("range-sensor") and
            engine.robot.requires("continuous-movement"))
-   return Practica3('Practica3', engine)
+    return Practica3('Practica3', engine)
