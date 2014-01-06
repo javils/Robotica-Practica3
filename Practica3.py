3821683b97dad457538bccb25be42eecadfe10f1
Juanjo <juanjose8993@gmail.com>
2014-01-06 20:02:02 +0100

    Añadida la función "step" y modificadas gran parte de las funciones.

--- Practica3.py
+++ Practica3.py
@@ -3,40 +3,65 @@ from pyrobot.brain import Brain
 import random
 
 class Individuo:
-  def __init__(self, IndId):
+  def __init__(self, IndId, padre):
     # Constantes
     self.CONTROL_BORROSO_SIZE = 12 # tamaño del array controlBorroso
     # Variables
     self.id = IndId #Id para diferenciar un individuo de otro
     self.calidad = 0 # calidad del individuo
     self.controlBorroso = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    self.initControlBorroso() # array con la funcion de pertenencia [X,X,X,Y,Y,Y,Z,Z,Z,Z,Z,Z] X = Error Y = D.Error Z = Salida
+    self.initControlBorroso(padre) # array con la funcion de pertenencia [X,X,X,Y,Y,Y,Z,Z,Z,Z,Z,Z] X = Error Y = D.Error Z = Salida
     self.probabilidad = 0 #probabilidad de ser elegido en la mutación
 
-  def initControlBorroso(self):
-    # Error
-    aux = [random.random() for col in range(3)]
-    aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
-    self.controlBorroso[0] = aux[0]
-    self.controlBorroso[1] = aux[1]
-    self.controlBorroso[2] = aux[2]
+  def initControlBorroso(self, padre):
+    if padre is None: #La primera población, sin padre
+        # Error
+        aux = [random.random() for col in range(3)]
+        aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
+        self.controlBorroso[0] = aux[0]
+        self.controlBorroso[1] = aux[1]
+        self.controlBorroso[2] = aux[2]
 
-    # Derivada del error
-    aux = [random.random() for col in range(3)]
-    aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
-    self.controlBorroso[3] = aux[0]
-    self.controlBorroso[4] = aux[1]
-    self.controlBorroso[5] = aux[2]
+        # Derivada del error
+        aux = [random.random() for col in range(3)]
+        aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
+        self.controlBorroso[3] = aux[0]
+        self.controlBorroso[4] = aux[1]
+        self.controlBorroso[5] = aux[2]
 
-    # Salida
-    aux = [random.random() for col in range(6)]
-    aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
-    self.controlBorroso[6] = aux[0]
-    self.controlBorroso[7] = aux[1]
-    self.controlBorroso[8] = aux[2]
-    self.controlBorroso[9] = aux[3]
-    self.controlBorroso[10] = aux[4]
-    self.controlBorroso[11] = aux[5]
+        # Salida
+        aux = [random.random() for col in range(6)]
+        aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
+        self.controlBorroso[6] = aux[0]
+        self.controlBorroso[7] = aux[1]
+        self.controlBorroso[8] = aux[2]
+        self.controlBorroso[9] = aux[3]
+        self.controlBorroso[10] = aux[4]
+        self.controlBorroso[11] = aux[5]
+    else: #Las poblaciones descendientes de otras
+        # Error
+        aux = [random.gauss(0,1) for col in range(3)]
+        aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
+        self.controlBorroso[0] = aux[0] + padre.controlBorroso[0]
+        self.controlBorroso[1] = aux[1] + padre.controlBorroso[1]
+        self.controlBorroso[2] = aux[2] + padre.controlBorroso[2]
+
+        # Derivada del error
+        aux = [random.gauss(0,1) for col in range(3)]
+        aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
+        self.controlBorroso[3] = aux[0] + padre.controlBorroso[3]
+        self.controlBorroso[4] = aux[1] + padre.controlBorroso[4]
+        self.controlBorroso[5] = aux[2] + padre.controlBorroso[5]
+
+        # Salida
+        aux = [random.gauss(0,1) for col in range(6)]
+        aux.sort() # ordenamos para que los puntos de corte de la funcion de pertenencia esten ordenados
+        self.controlBorroso[6] = aux[0] + padre.controlBorroso[6]
+        self.controlBorroso[7] = aux[1] + padre.controlBorroso[7]
+        self.controlBorroso[8] = aux[2] + padre.controlBorroso[8]
+        self.controlBorroso[9] = aux[3] + padre.controlBorroso[9]
+        self.controlBorroso[10] = aux[4] + padre.controlBorroso[10]
+        self.controlBorroso[11] = aux[5] + padre.controlBorroso[11]
 
 
 class Control(Brain):
@@ -53,32 +78,45 @@ class Control(Brain):
     self.ind = 0 #individuo en el que estamos
     self.poblacion = range(0, self.MAX_IND)
     self.elite = Individuo(-1)
-    probTotal = 0
 
-    # Inicializamos la primera población
-    for i in range(self.MAX_IND):
-      self.poblacion[i] = Individuo(i)
-      self.poblacion[i].calidad = 1 # Para que no crashe por dividir entre 0 mas adelante
-      self.probTotal = probTotal + self.poblacion[i].calidad
+    self.generarPoblacion(esPrimeraGeneracion=True)
 
-      # Elitismo
-      if (i == 0):
-        self.elite = self.poblacion[i]
-      elif (self.elite.calidad < self.poblacion[i].calidad):
-        self.elite = self.poblacion[i]
-
-    # Asignamos la probabilidad que tiene de ser elegido cuando se vaya a mutar.
-    for i in range(self.MAX_IND):
-      self.poblacion[i].probabilidad = float((self.poblacion[i].calidad)/self.probTotal)
-
-  # Asigna calidad a un individuo
-  def setCalidad(self):
-    return self.MAX_ITR/self.itr
 
   def step(self):
-      self.itr = 0
+      if self.itr == self.MAX_ITR or luz_alcanzada:
+          self.setCalidad(self.poblacion[self.ind], self.itr)
+          self.ind = self.ind + 1
+          if self.ind == self.MAX_IND:
+              self.ind = 0
+              self.gen = self.gen + 1
+              if self.gen == self.MAX_GEN:
+                  done = 1
+              else:
+                  self.generarPoblacion(esPrimeraGeneracion=False)
+          self.colocarIndividuo(self.ind)
+          self.itr = 0
+      else:
+          t,r = determineMove(self.poblacion[self.ind])
+          self.robot.move(t,r)
+          self.itr = self.itr + 1
+
+
+  def generarPoblacion(self, esPrimeraGeneracion):
+      if esPrimeraGeneracion:
+          for i in range(self.MAX_IND):
+            self.poblacion[i] = Individuo(i, None)
+      else:
+          for i in range(self.MAX_IND):
+            self.poblacion[i] = Individuo(i, self.poblacion[self.elite])
+
+# Asigna calidad a un individuo
+  def setCalidad(self, individuoActual, iteracion):
+    individuoActual.calidad =  1/iteracion
+    if individuoActual.calidad > self.elite.calidad:
+        self.elite = individuoActual.calidad
+
 
 def INIT(engine):
     assert (engine.robot.requires("range-sensor")
-			and engine.robot.requires("continuous-movement")
+			and engine.robot.requires("continuous-movement"))
     return Control('Control', engine)
